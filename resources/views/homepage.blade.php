f41132401e12522823a395c7a3d4323fae4fef12
Xen-kun <stevedaleja2001@gmail.com>
2024-11-16 06:46:09 +0800

    changes

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -4,17 +4,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://fonts.bunny.net/css2?family=Kanit:wght@400;600;700&display=swap" rel="stylesheet">
     <title>Veloce</title>
-    <link rel="stylesheet" href="{{ asset('dist/assets/app.css') }}">
-
-    <!-- Google tag (gtag.js) -->
-    {{-- <script async src="https://www.googletagmanager.com/gtag/js?id=G-Q4CT93RWVC"></script>
-    <script>
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-
-        gtag('config', 'G-Q4CT93RWVC');
-    </script> --}}
+    <link rel="stylesheet" href="{{ asset('/dist/assets/app.css') }}">
 </head>
 <body>
     {{-- header --}}
