e099d14e7857e7b607626fcd3609336912d63515
Xen-kun <stevedaleja2001@gmail.com>
2024-11-15 19:50:51 +0800

    changes to run sass

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,14 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-  plugins: [
-    laravel({
-      input: ['resources/js/app.js', 'resources/css/app.css'],
-      refresh: true,
-    }),
-  ],
-  build: {
-    outDir: 'public/dist',  // This changes the output folder to 'public/dist'
-  },
+    plugins: [
+        laravel({
+            input: [
+                'resources/css/app.css',
+                'resources/scss/main.scss',
+                'resources/js/app.js',
+            ],
+            refresh: true
+        }),
+    ],
 });
