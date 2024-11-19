9d9b4c05de2283cc65bc69caf0b0165742800393
Xen-kun <stevedaleja2001@gmail.com>
2024-11-19 13:13:45 +0800

    changes

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,11 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ['resources/css/app.css', 'resources/js/app.js'], // Entry files
             refresh: true,
         }),
     ],
+    build: {
+        outDir: 'dist', // The folder where the production files will be generated
+    },
 });
