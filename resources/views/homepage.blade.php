2ad9322d5703f3ef27b58000a4043d533db67084
Xen-kun <stevedaleja2001@gmail.com>
2024-12-01 00:38:26 +0800

    changes

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -4,7 +4,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://fonts.bunny.net/css2?family=Kanit:wght@400;600;700&display=swap" rel="stylesheet">
     <title>Veloce</title>
-    @vite(['resources/scss/main.scss', 'resources/css/app.css', 'resources/js/app.js'])
+    @vite(['resources/scss/main.scss', 'resources/js/app.js'])
     <!-- Google tag (gtag.js) -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=G-NBBDKGW5MP"></script>
     <script>
